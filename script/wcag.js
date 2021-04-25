bf527980983ef422ca09b0ab61a0f34e589e0143
Eric Eggert <mail@yatil.net>
2021-04-25 17:52:27 +0200

    Add (english) to understanding & quickref
    Signed-off-by: Eric Eggert <mail@yatil.net>

--- script/wcag.js
+++ script/wcag.js
@@ -13,7 +13,7 @@ function linkUnderstanding() {
 		var pathFrag = titleToPathFrag(heading);
 		var el = document.createElement("div");
 		el.setAttribute("class", "doclinks");
-		el.innerHTML = "<a href=\"" + understandingBaseURI + pathFrag + ".html\">" + heading + " verstehen</a> <span class=\"screenreader\">|</span> <br /><a href=\"https://www.w3.org/WAI/WCAG" + version + "/quickref/#" + pathFrag + "\">" + heading + " erfüllen</a>";
+		el.innerHTML = "<a href=\"" + understandingBaseURI + pathFrag + ".html\">" + heading + " verstehen (englisch)</a> <span class=\"screenreader\">|</span> <br /><a href=\"https://www.w3.org/WAI/WCAG" + version + "/quickref/#" + pathFrag + "\">" + heading + " erfüllen (englisch)</a>";
 		node.insertBefore(el, node.children[1]);
 	})
 }
